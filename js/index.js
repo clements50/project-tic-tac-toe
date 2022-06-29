5574b4e034d0bd17b7072b5eba5d9e3e3d60d0a3
Cody Clements <subliminalverses2@gmail.com>
2022-06-29 14:13:31 -0500

    finish main logic

--- js/index.js
+++ js/index.js
@@ -18,10 +18,9 @@ const players = function (playerName, playerSymbol) {
 };
 
 const displayController = (function () {
-  const playerOne = players('Chris', 'X');
-  const playerTwo = players('Orianna', 'O');
+  const playerOne = players('PlayerX', 'X');
+  const playerTwo = players('PlayerO', 'O');
   let currentPlayer = playerOne;
-
   let winner = false;
   let spotsLeft = 9;
 
@@ -30,20 +29,23 @@ const displayController = (function () {
   };
 
   const placeMark = function () {
-    gameBoard.tiles.forEach((tile, i) => {
+    gameBoard.tiles.forEach((tile) => {
       tile.addEventListener('click', function tileEvent() {
         if (tile.textContent === '' && winner != true) {
           spotsLeft -= 1;
           const tileDataAttr = tile.dataset.index;
+          console.log(gameBoard.board);
           gameBoard.board.splice(tileDataAttr, 1, currentPlayer.playerSymbol);
-          gameBoard.renderBoard();
           checkWinner();
           turn();
-          console.log(winner);
+          gameBoard.renderBoard();
         }
-        if (spotsLeft < 1) {
+
+        if (spotsLeft <= 0 && winner != true ) {
+          const modal = document.querySelector('.modal-background');
           const winnerMessage = document.querySelector('.winner-message');
-          winnerMessage.textContent = 'Its A Tie'
+          modal.classList.add('modal-active');
+          winnerMessage.textContent = 'Its A Tie';
         }
       });
     });
@@ -67,11 +69,26 @@ const displayController = (function () {
           gameBoard.board[combo[2]] === currentPlayer.playerSymbol
         ) {
           const winnerMessage = document.querySelector('.winner-message');
-          winnerMessage.textContent = `${currentPlayer.playerName} Wins !`
+          const modal = document.querySelector('.modal-background');
+          modal.classList.add('modal-active');
+          winnerMessage.textContent = `${currentPlayer.playerName} Wins !`;
           winner = true;
         }
       });
     }
+
+    const playAgainBtn = document.querySelector('.play-again-btn');
+    playAgainBtn.addEventListener('click', function () {
+      const modal = document.querySelector('.modal-background');
+      gameBoard.board.splice(0, gameBoard.board.length, '', '', '', '', '', '', '', '', '');
+      currentPlayer = playerOne;
+      gameBoard.tiles.forEach((tile) => {
+        tile.textContent = '';
+      });
+      spotsLeft = 9;
+      winner = false;
+      modal.classList.remove('modal-active');
+    });
   };
 
   return { placeMark };
@@ -83,7 +100,7 @@ const playBtn = document.querySelector('.play-btn');
 const playGameContainer = document.querySelector('.playgame');
 const gameboardContainer = document.querySelector('.gameboard');
 
-playBtn.addEventListener('click', function() {
+playBtn.addEventListener('click', function () {
   playGameContainer.style.display = 'none';
   gameboardContainer.style.display = 'grid';
-})
+});
